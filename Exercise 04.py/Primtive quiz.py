d9295febda5d5bdb55d5005d27bc65eb95d8d654
Sxottcobolt-MH <sxottcobolt@gmail.com>
2024-11-07 11:16:54 +0400

    Create Primtive quiz.py

---
+++ Exercise 04.py/Primtive quiz.py
@@ -0,0 +1,8 @@
+Answer = input("What is the capital of la France? ")
+if Answer == "Paris":
+    print("correct")
+else:
+    print("incorrect")
+
+#It questions the user, 'What is the capital of La France'.
+#The program makes use of a if else statement, as a way to tell the user if their answer is correct or incorrect.
