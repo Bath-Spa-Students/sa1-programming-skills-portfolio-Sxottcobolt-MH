59007e729fd383543708f740d6484c36587182bc
Sxottcobolt-MH <sxottcobolt@gmail.com>
2024-11-07 11:17:57 +0400

    Create Primtive quiz Advanced.py

---
+++ Exercise 04.py/Primtive quiz Advanced.py
@@ -0,0 +1,62 @@
+Answer1 = input("What is the capital of la France? ").lower()                  #Asks the user 'what is the capital of France'.
+if Answer1 == "paris":
+    print("Correct, the capital of La France is paris.")
+else:
+    print("Incorrect, the capital of La France is paris.")
+
+Answer2 = input("What is the capital of Deutschland? ").lower()                #Asks the user 'what is the capital of Germany'.
+if Answer2 == "berlin":
+    print("Correct, the capital of Deutschland is berlin.")
+else:
+    print("Incorrect, the capital of Deutschland is berlin.")
+
+Answer3 = input("What is the capital of Italia? ").lower()                    #Asks the user 'what is the capital of Italy'.
+if Answer3 == "rome":
+    print("Correct, the capital of Italia is rome.")
+else:
+    print("Incorrect, the capital of Italia is rome.")
+
+Answer4 = input("What is the capital of Greacia? ").lower()                   #Asks the user 'what is the capital of Greece'.
+if Answer4 == "athens":
+    print("Correct, the capital of Greacia is athens.")
+else:
+    print("Incorrect, the capital of Greacia is athens.")
+
+Answer5 = input("What is the capital of Sakartvelo? ").lower()                #Asks the user 'what is the capital of Gerogia'.
+if Answer5 == "tbilisi":
+    print("Correct, the capital of Sakartvelo is tbilisi.")
+else:
+    print("Incorrect, the capital of Sakartvelo is tbilisi.")
+
+Answer6 = input("What is the capital of Republika Srbija? ").lower()           #Asks the user 'what is the capital of Serbia'.
+if Answer6 == "belgrade":
+    print("Correct, the capital of Republika Srbija is belgrade.")
+else:
+    print("Incorrect, the capital of Republika Srbija is belgrade.")
+
+Answer7 = input("What is the capital of England? ").lower()                   #Asks the user 'what is the capital of England'.
+if Answer7 == "london":
+    print("Correct, the capital of England is london.")
+else:
+    print("Incorrect, the capital of England is london.")
+
+Answer8 = input("What is the capital of Norge? ").lower()                     #Asks the user 'what is the capital of Norway'.
+if Answer8 == "oslo":
+    print("Correct, the capital of Norge is oslo.")
+else:
+    print("Incorrect, the capital of Norge is oslo.")
+
+Answer9 = input("What is the capital of România? ").lower()                  #Asks the user 'what is the capital of Romania'.
+if Answer9 == "bucharest":
+    print("Correct, the capital of România is bucharest.")
+else:
+    print("Incorrect, the capital of România is bucharest.")
+
+Answer10 = input("What is the capital of Polska? ").lower()            #Asks the user 'what is the capital of Poland'.
+if Answer10 == "warsaw":
+    print("Correct, the capital of Polksa is warsaw.")
+else:
+    print("Incorrect, the capital of Polska is warsaw")
+
+
+
