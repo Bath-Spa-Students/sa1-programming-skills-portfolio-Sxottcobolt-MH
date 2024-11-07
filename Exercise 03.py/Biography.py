2925864828dd15684a391dfeaa6899f903f5f120
Sxottcobolt-MH <sxottcobolt@gmail.com>
2024-11-07 11:16:19 +0400

    Biography

---
+++ Exercise 03.py/Biography.py
@@ -0,0 +1,10 @@
+Info = {
+    "name" : "Adam",
+    "hometown" : "Miridf",
+    "age" : 17
+}
+
+print(*Info.values(), sep='\n')
+
+#It prints out the dictionary. A dictionary is used to store data values in key pairs.
+#Each of them are printed on separate lines using a single print statement.
