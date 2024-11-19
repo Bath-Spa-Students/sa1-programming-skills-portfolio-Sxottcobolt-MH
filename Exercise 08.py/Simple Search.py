a5850ff2e261f3a74ce6ff89c07de9462926199d
Sxottcobolt-MH <sxottcobolt@gmail.com>
2024-11-19 11:23:33 +0400

    Create Simple Search.py

---
+++ Exercise 08.py/Simple Search.py
@@ -0,0 +1,5 @@
+ThisList = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+print(ThisList[4])
+
+#Every item in a list has an index number, always starting from 0
+#To print a specific item from a list, refer each item from its index number.
