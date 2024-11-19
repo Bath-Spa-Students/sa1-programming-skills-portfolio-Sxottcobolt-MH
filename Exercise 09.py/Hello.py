64db6f0e77fb7791e615aedc5c7512e748e0e44b
Sxottcobolt-MH <sxottcobolt@gmail.com>
2024-11-19 11:23:40 +0400

    Create Hello.py

---
+++ Exercise 09.py/Hello.py
@@ -0,0 +1,9 @@
+def hello(): # Defining the function
+    print("Hello")
+def main(): 
+    hello()
+if __name__ == "__main__": # Calling out the function
+    main()
+
+#A function is code that is reuseable, which can only run whne it is called out.
+#As output, it can return data.
