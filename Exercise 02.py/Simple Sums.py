9beca913732cc5d9eb7455a0307cb18f947c7eb5
Sxottcobolt-MH <sxottcobolt@gmail.com>
2024-11-21 20:23:47 +0400

    Update Simple Sums.py

--- Exercise 02.py/Simple Sums.py
+++ Exercise 02.py/Simple Sums.py
@@ -3,5 +3,5 @@ b = 10
 c = a + b
 print(c)
 
-#The program first adds both a and b to get c, which in this case, 8 + 10 = 12.
+#The program first adds both a and b to get c, which in this case, 8 + 10 = 18.
 #The program makes use of the + operator to get c.
